e361f57bcdafbd5949af5d77ce1412c2490be31b
ANASTAD2000 <anasbaclibr@gmai.com>
2025-02-14 11:27:19 +0100

    fix errour in connection whit databas

--- src/main/java/stockmaster20/DBConnection.java
+++ src/main/java/stockmaster20/DBConnection.java
@@ -1,29 +1,34 @@
-package stockmaster20;
-
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/stockmaster";
-    private static final String USER = "root"; 
-    private static final String PASSWORD = "admin";
+    private static final String JDBC_URL = "jdbc:mysql://localhost:8089/stockmaster?useSSL=false&serverTimezone=UTC";
+    private static final String JDBC_USERNAME = "root";
+    private static final String JDBC_PASSWORD = "admin";
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    protected Connection getConnection() {
+        Connection connection = null;
+        try {
+             
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace(); 
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); 
+        }
+        return connection;
     }
 
     public static void main(String[] args) {
-        try (Connection conn = DBConnection.getConnection()) {
-            if (conn != null) {
-                System.out.println("✅ Connection successful!");
-            } else {
-                System.out.println("❌ Connection failed!");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+        DBConnection db = new DBConnection();
+        Connection conn = db.getConnection();
+        if (conn != null) {
+            System.out.println("Connection successful!");
+        } else {
+            System.out.println("Connection failed!");
         }
     }
 }
